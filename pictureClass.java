6530439c1a3fdd8e9f8ff9510f51f09db85fde35
lr101 <48615489+lr101@users.noreply.github.com>
2020-08-03 18:28:30 +0200

    Add files via upload

---
+++ pictureClass.java
@@ -0,0 +1,36 @@
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class pictureClass {
+    private ArrayList<Image> list = new ArrayList<>();
+    private double ratio;
+    private int numUsed = 0;
+
+    public  pictureClass( Image image){
+        ratio = (double)image.getHeight() / (double) image.getWidth();
+        list.add(image);
+    }
+
+
+    public ArrayList<Image> getImageList(){
+        return  list;
+    }
+    public int size(){return list.size();}
+    public int getNumUsed(){return numUsed;}
+    public void plusUsed(){numUsed++;}
+    public void clearNumUsed(){
+        numUsed = 0;
+    }
+
+    public boolean addImage(Image image){
+        if (((double)image.getHeight() / (double) image.getWidth()) == ratio){
+            list.add(image);
+            return true;
+        }
+        return false;
+    }
+
+    public double ratio (){
+        return ratio;
+    }
+}
